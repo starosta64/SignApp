62b4b113b16260bcedaae11a2bd7854579da230c
starosta64 <kro00@mail.ru>
2021-06-04 14:57:38 +0300

    Initial Commit

---
+++ SignApp/SignAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignAppApp.swift
+//  SignApp
+//
+//  Created by user on 04.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct SignAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
